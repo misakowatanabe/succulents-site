9ca086cc03f2d3573b489eda9dd0ba17dfba943a
Misako Watanabe <misakolani@gmail.com>
2021-11-22 19:43:04 +0100

    separated cartPreview components for readability

--- src/components/common/CartPreview.tsx
+++ src/components/common/CartPreview.tsx
@@ -1,38 +1,11 @@
 import React, { useContext } from "react";
 import { AppContext } from "../../context/Context";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import { PopperPlacementType } from "@material-ui/core/Popper";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import RichTooltip from "./RichTooltip";
 import CartPreviewContents from "./CartPreviewContents";
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: "flex",
-      boxShadow: "none",
-      borderRadius: 0,
-      overFlow: "hidden",
-      paddingTop: "10px",
-    },
-
-    details: {
-      display: "flex",
-      flexDirection: "column",
-    },
-    content: {
-      flex: "1 0 auto",
-      paddingLeft: 10,
-      paddingTop: 0,
-      paddingBottom: "0 !important",
-    },
-    cover: {
-      width: 100,
-      height: 100,
-    },
-  })
-);
+import { useStyles } from "./CartPreviewStyles";
 
 export default function CartPreview() {
   const [open, setOpen] = React.useState(false);
@@ -40,18 +13,24 @@ export default function CartPreview() {
 
   const classes = useStyles();
 
-  const handleClick =
-    (newPlacement: PopperPlacementType) =>
-    () => {
-      setOpen((prev) => placement !== newPlacement || !prev);
-      setPlacement(newPlacement);
-    };
+  const handleClick = (newPlacement: PopperPlacementType) => () => {
+    setOpen((prev) => placement !== newPlacement || !prev);
+    setPlacement(newPlacement);
+  };
 
   const handleClickAway = () => {
     setOpen(false);
   };
 
   const { state } = useContext(AppContext);
+
+  const CartState = () => {
+    if (state.shoppingCart < 100) {
+      return <div className="cart-state">{state.shoppingCart}</div>;
+    } else {
+      return <div className="cart-state">99+</div>;
+    }
+  };
 
   return (
     <ClickAwayListener onClickAway={handleClickAway}>
@@ -72,11 +51,7 @@ export default function CartPreview() {
                   : "cart-state_container-more-than-hundred"
               }
             >
-              {state.shoppingCart < 100 ? (
-                <div className="cart-state">{state.shoppingCart}</div>
-              ) : (
-                <div className="cart-state">99+</div>
-              )}
+              <CartState />
             </div>
             <button onClick={handleClick("bottom-end")} className="cart-icon">
               <ShoppingCartIcon style={{ fontSize: "34px" }} />
--- src/components/common/CartPreviewContents.tsx
+++ src/components/common/CartPreviewContents.tsx
@@ -1,40 +1,8 @@
-import React, { useContext } from "react";
-import { NavLink } from "react-router-dom";
-import { AppContext } from "../../context/Context";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import React from "react";
 import Fade from "@material-ui/core/Fade";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
-import GoToCartButton from "./GoToCartButton"; 
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: "flex",
-      boxShadow: "none",
-      borderRadius: 0,
-      overFlow: "hidden",
-      paddingTop: "10px",
-    },
-
-    details: {
-      display: "flex",
-      flexDirection: "column",
-    },
-    content: {
-      flex: "1 0 auto",
-      paddingLeft: 10,
-      paddingTop: 0,
-      paddingBottom: "0 !important",
-    },
-    cover: {
-      width: 100,
-      height: 100,
-    },
-  })
-);
+import GoToCartButton from "./GoToCartButton";
+import CartPreviewContentsSwitch from "./CartPreviewContentsSwitch";
 
 type CartPreviewContentsProps = {
   onClick: (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
@@ -43,10 +11,6 @@ type CartPreviewContentsProps = {
 export default function CartPreviewContents({
   onClick,
 }: CartPreviewContentsProps) {
-  const classes = useStyles();
-
-  const { state } = useContext(AppContext);
-
   const isMobile = useMediaQuery("(max-width:599px)");
 
   return (
@@ -60,53 +24,17 @@ export default function CartPreviewContents({
                 : "cart-preview_container_bigger-screen"
             }
           >
-              <div className="title-in-cart-preview">YOUR SHOPPING CART</div>
-              <div
-                className={
-                  isMobile
-                    ? "item-in-cart-preview_mobile"
-                    : "item-in-cart-preview_bigger-screen"
-                }
-              >
-                {state.products[0] == null ? (
-                  <div className="no-product-in-cart-preview">
-                    There is no product in your cart.
-                  </div>
-                ) : (
-                  <div>
-                    {state.products.map((productInCart) => (
-                      <Card className={classes.root} key={productInCart.id}>
-                        <NavLink
-                          to={`/product/${productInCart.category}/${productInCart.id}`}
-                          onClick={onClick}
-                        >
-                          <CardMedia
-                            className={classes.cover}
-                            image={productInCart.image}
-                            title="Product"
-                          />
-                        </NavLink>
-                        <div className={classes.details}>
-                          <CardContent className={classes.content}>
-                            <div>
-                              <div className="product-name-in-cart-preview">
-                                {productInCart.name}
-                              </div>
-                              <div className="sub-info-in-cart-preview">
-                                {productInCart.price} kr
-                              </div>
-                              <div className="sub-info-in-cart-preview">
-                                Quantity: {productInCart.quantity}
-                              </div>
-                            </div>
-                          </CardContent>
-                        </div>
-                      </Card>
-                    ))}
-                  </div>
-                )}
-              </div>
-              <GoToCartButton onClick={onClick} />
+            <div className="title-in-cart-preview">YOUR SHOPPING CART</div>
+            <div
+              className={
+                isMobile
+                  ? "item-in-cart-preview_mobile"
+                  : "item-in-cart-preview_bigger-screen"
+              }
+            >
+              <CartPreviewContentsSwitch onClick={onClick} />
+            </div>
+            <GoToCartButton onClick={onClick} />
           </div>
         </div>
       </Fade>
